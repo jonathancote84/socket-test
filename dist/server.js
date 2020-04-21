98f1c4d27bd141a7c40916aed5d943250711166f
Jonathan Cote <jonathancote92@gmail.com>
2020-04-20 21:45:37 -0500

    initial with 2 way

---
+++ dist/server.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const path = require("path");
+const app = express();
+app.set("port", process.env.PORT || 3000);
+let http = require("http").Server(app);
+// set up socket.io and bind it to our 
+// http server. 
+let io = require("socket.io")(http);
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("./client/index.html"));
+});
+// whenever a user connects on port 3000 via 
+// a websocket, log that a user has connected
+io.on("connection", function (socket) {
+    console.log("a user connected");
+    // whenever we recieve a 'message' we log it out
+    socket.on("message", function (message) {
+        console.log(message);
+        // echo the message back down the 
+        // websocket connection
+        socket.emit("message", message);
+    });
+});
+const server = http.listen(3000, function () {
+    console.log("listening on *:3000");
+});
+//# sourceMappingURL=server.js.map
